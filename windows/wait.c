0089c7b5bffc141e955945f0c7d7a9e9ce036d87
Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
2014-04-14 22:38:26 +0300

    Add function waitpid to windows/wait.c

--- windows/wait.c
+++ windows/wait.c
@@ -13,3 +13,36 @@ int __pipedes[2];
 
     return _pipe(__pipedes, 0, _O_BINARY | _O_NOINHERIT);
 }
+
+pid_t
+waitpid(pid_t pid, int *stat_loc, int options)
+{
+    HANDLE hProcess;
+    DWORD status;
+
+    if ((options & ~(WNOHANG | WUNTRACED)) != 0) {
+        _set_errno(EINVAL);
+        return (pid_t)-1;
+    }
+    if ((pid == -1) || (pid == -2)) {
+        errno = ECHILD;
+        return -1;
+    }
+    hProcess = OpenProcess(SYNCHRONIZE, FALSE, pid);
+    if (hProcess == NULL) {
+        _set_errno(EINVAL);
+        return -1;
+    }
+    if (WaitForSingleObject(hProcess, (DWORD)(-1L)) == WAIT_FAILED) {
+        _set_errno(EINVAL);
+        return -1;
+    }
+    if (!GetExitCodeProcess(hProcess, &status)) {
+        _set_errno(EINVAL);
+        return -1;
+    }
+    if (!stat_loc)
+        *stat_loc = status;
+    CloseHandle(hProcess);
+    return pid;
+}
