5aa71d312e1dda247e1b64c7d2abb7cb40276674
Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
2014-04-14 17:40:33 +0300

    Add several defines to linux/filter.h

--- windows/linux/filter.h
+++ windows/linux/filter.h
@@ -9,3 +9,16 @@ struct sock_fprog {     /* Required for SO_ATTACH_FILTER. */
          unsigned short          len;    /* Number of filter blocks */
          struct sock_filter *filter;
 };
+
+/* Socket filtering */
+#define SO_ATTACH_FILTER        26
+#define SO_DETACH_FILTER        27
+
+#define SO_PEERNAME     28
+#define SO_TIMESTAMP        29
+#define SCM_TIMESTAMP       SO_TIMESTAMP
+
+#define SO_PEERSEC      30
+#define SO_PASSSEC      34
+#define SO_TIMESTAMPNS      35
+#define SCM_TIMESTAMPNS     SO_TIMESTAMPNS
