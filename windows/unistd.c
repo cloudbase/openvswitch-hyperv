fd622b7952b0605f977279ccef72d2c930700c6f
Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
2014-04-14 22:23:18 +0300

    Add function strsep to unistd.c and also the file windows/unistd.c

---
+++ windows/unistd.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+char* strsep(char** stringp, const char* delim)
+{
+	char* start = *stringp;
+	char* p;
+
+	p = (start != NULL) ? strpbrk(start, delim) : NULL;
+
+	if (p == NULL)
+	{
+		*stringp = NULL;
+	}
+	else
+	{
+		*p = '\0';
+		*stringp = p + 1;
+	}
+
+	return start;
+}
