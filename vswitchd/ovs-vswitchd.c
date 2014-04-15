be5e3d7256442b5ccb6b652a1d62df30bdd1194f
Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>
2014-04-15 17:34:20 +0300

    Add default connect to localhost and port 5559. Add wsastartup and remote_io for debugging

--- vswitchd/ovs-vswitchd.c
+++ vswitchd/ovs-vswitchd.c
@@ -33,12 +33,20 @@
 #include "dpif.h"
 #include "dummy.h"
 #include "leak-checker.h"
+#ifdef _WIN32
+#include "lib/memory.h"
+#else
 #include "memory.h"
+#endif
 #include "netdev.h"
 #include "openflow/openflow.h"
 #include "ovsdb-idl.h"
 #include "poll-loop.h"
+#ifdef _WIN32
+#include "lib/process.h"
+#else
 #include "process.h"
+#endif
 #include "signals.h"
 #include "simap.h"
 #include "stream-ssl.h"
@@ -53,6 +61,10 @@
 #include "lib/vswitch-idl.h"
 #include "worker.h"
 
+#ifdef _WIN32
+#define HAVE_MLOCKALL 0
+#undef HAVE_MLOCKALL
+#endif
 VLOG_DEFINE_THIS_MODULE(vswitchd);
 
 /* --mlockall: If set, locks all process memory into physical RAM, preventing
@@ -67,6 +79,29 @@ static void usage(void) NO_RETURN;
 int
 main(int argc, char *argv[])
 {
+#ifdef _WIN32
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+
+	/* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+	wVersionRequested = MAKEWORD(2, 2);
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		/* Tell the user that we could not find a usable */
+		/* Winsock DLL.                                  */
+		printf("WSAStartup failed with error: %d\n", err);
+		return 1;
+	}
+#if __USE_REMOTE_IO_NL_DEVICE
+	if (!RemoteIo_Init())
+	{
+		printf("RemoteIo_Init failed\n");
+		return -1;
+	}
+#endif
+#endif
     char *unixctl_path = NULL;
     struct unixctl_server *unixctl;
     struct signal *sighup;
@@ -95,7 +130,9 @@ main(int argc, char *argv[])
 #endif
     }
 
-    worker_start();
+#ifndef _WIN32
+	worker_start();
+#endif
 
     retval = unixctl_server_create(unixctl_path, &unixctl);
     if (retval) {
@@ -104,14 +141,18 @@ main(int argc, char *argv[])
     unixctl_command_register("exit", "", 0, 0, ovs_vswitchd_exit, &exiting);
 
     bridge_init(remote);
-    free(remote);
+#ifndef _WIN32
+	free(remote);
+#endif
 
     exiting = false;
     while (!exiting) {
         worker_run();
-        if (signal_poll(sighup)) {
-            vlog_reopen_log_file();
-        }
+#ifndef _WIN32
+		if (signal_poll(sighup)) {
+			vlog_reopen_log_file();
+		}
+#endif
         memory_run();
         if (memory_should_report()) {
             struct simap usage;
@@ -141,6 +182,13 @@ main(int argc, char *argv[])
     bridge_exit();
     unixctl_server_destroy(unixctl);
     signal_unregister(sighup);
+#if __USE_REMOTE_IO_NL_DEVICE
+	if (!RemoteIo_Uninit())
+	{
+		printf("RemoteIo_Uninit failed\n");
+		return -1;
+	}
+#endif
 
     return 0;
 }
@@ -235,7 +283,11 @@ parse_options(int argc, char *argv[], char **unixctl_pathp)
 
     switch (argc) {
     case 0:
-        return xasprintf("unix:%s/db.sock", ovs_rundir());
+#ifndef _WIN32
+		return xasprintf("unix:%s/db.sock", ovs_rundir());
+#else
+		return xasprintf("tcp:127.0.0.1:5559", ovs_rundir());
+#endif
 
     case 1:
         return xstrdup(argv[0]);
